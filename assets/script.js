c58da5c9e5e1b11583f83cb5fb6c80834f722b30
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-27 14:35:44 +0200

    add const 'endTime' and 'count'

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,14 @@ Esiste un oggetto JS in grado di gestire le date?
 Esistono dei metodi per trasformare una data in millisecondi?
 */
 
-const now = new Date();
+// Creare la data attuale in millisecondi
+const now = new Date().getTime();
 console.log(now);
+
+// Creare la data del 28/04/2023 alle 9:30
+const endTime = new Date('2023-04-28T09:30:00').getTime();
+console.log(endTime);
+
+// La differenza tra le due date è il tempo di attesa
+const count = endTime - now;
+console.log(count);
