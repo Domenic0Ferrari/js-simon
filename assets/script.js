c18b57eb2aeae439c07bbbeb55def7c5dbcf244d
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-27 14:29:44 +0200

    add script to index and add newDate

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+/*
+Sfruttiamo le -timing functions- per fare il conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+*/
+
+/*
+Da quante ore è composto un giorno?
+Da quanti minuti è composta un'ora?
+Da quanti secondi è composto un minuto?
+Da quanti millisecondi è composto un secondo?
+Quanti millisecondi mi separano da domani alle 9:30?
+Esiste un oggetto JS in grado di gestire le date?
+Esistono dei metodi per trasformare una data in millisecondi?
+*/
+
+const now = new Date();
+console.log(now);
